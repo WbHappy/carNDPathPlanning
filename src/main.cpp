f9782c0e7367a218f7378cbccbc5a3dbcf5b8697
Wagdi BEN YAALA <wagdi.ben@gmail.com>
2019-09-06 08:21:29 +0200

    moving on with implem

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,9 @@ using nlohmann::json;
 using std::string;
 using std::vector;
 
+const double MAX_SPEED = 49.5;
+const double MAX_ACCELERATION = .2;
+
 int main() {
   uWS::Hub h;
 
@@ -52,7 +55,13 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
+  // Car current lane. Init at middle lane.
+  int current_lane = 1;
+
+  // Car current vel. Init at 0
+  double current_vel = 0.0;
+
+  h.onMessage([&current_vel, &current_lane, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
                &map_waypoints_dx,&map_waypoints_dy]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
@@ -100,13 +109,73 @@ int main() {
            *   sequentially every .02 seconds
            */
 
-          double dist_inc = 0.5;
-          for (int i = 0; i < 50; ++i) {
-            next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
-            next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
+          int path_size = previous_path_x.size();
+
+          // First Check all cars positions and 
+          // check if cars are close and in our lane, on left or on the right lane
+          bool car_ahead = false;
+          bool car_left = false;
+          bool car_righ = false;
+          double distance_threshold = 30;
+          for ( int i = 0; i < sensor_fusion.size(); i++ ) {
+              float d = sensor_fusion[i][6];
+              int car_lane = -999;
+              // center left lane 0  : 2
+              // center middle lane 1  : 6
+              // center right lane 2  : 10
+              if ( 0 < d && d < 4 ) { // lane is always 4m wide
+                car_lane = 0;
+              } else if ( 4 < d && d < 8 ) {
+                car_lane = 1;
+              } else if ( 8 < d && d < 12 ) {
+                car_lane = 2;
+              }
+              if (car_lane < 0) {
+                std::cout << "check car lane - something went wrong !" << std::endl;
+                continue;
+              }
+              // Find selected car speed.
+              double vx = sensor_fusion[i][3];
+              double vy = sensor_fusion[i][4];
+              double speed = sqrt(vx*vx + vy*vy);
+              double neighbor_car_s = sensor_fusion[i][5];
+              // distance interpolating neighbor car s position
+              neighbor_car_s += ((double)path_size*0.02*speed);
+
+              if ( car_lane == current_lane ) {
+                // Car is in our lane.
+                car_ahead |= neighbor_car_s > car_s && neighbor_car_s - car_s < distance_threshold;
+              } else if ( car_lane - current_lane == -1 ) {
+                // Car is on our left
+                car_left |= car_s - distance_threshold < neighbor_car_s && car_s + distance_threshold > neighbor_car_s;
+              } else if ( car_lane - current_lane == 1 ) {
+                // Car is on our right
+                car_righ |= car_s - distance_threshold < neighbor_car_s && car_s + distance_threshold > neighbor_car_s;
+              }
           }
 
-
+          // handle our speed depending on neighbor cars
+          double speed_adjustment = 0;
+          if ( car_ahead ) { // Car ahead
+            if ( !car_left && current_lane > 0 ) {
+              // if there is no car left and there is a left lane.
+              current_lane--; // Change lane left.
+            } else if ( !car_righ && current_lane != 2 ){
+              // if there is no car right and there is a right lane.
+              current_lane++; // Change lane right.
+            } else {
+              speed_adjustment -= MAX_ACCELERATION;
+            }
+          } else {
+            if ( current_lane != 1 ) { // if we are not on the center lane.
+              if ( ( current_lane == 0 && !car_righ ) || ( current_lane == 2 && !car_left ) ) {
+                current_lane = 1; // Back to center.
+              }
+            }
+            if ( current_vel < MAX_SPEED ) {
+              speed_adjustment += MAX_ACCELERATION;
+            }
+          }          
           // ---------------
 
           msgJson["next_x"] = next_x_vals;
